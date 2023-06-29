b6933679d103c39621064c10cfa5f4466a716767
Pebaz <pebazium@gmail.com>
2023-06-29 00:16:53 -0700

    Initial Commit

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # tabler
+
+> Convert a list of columns and rows into a GUI Table
+
+```python
+from tabler import tabler
+
+tabler([
+    ['Name', 'URL'],
+    ['Microsoft', 'https://www.microsoft.com'],
+    ['Google', 'https://www.google.com'],
+    ['Apple', 'https://www.apple.com'],
+    title='Companies'
+])
+```
+
+![Screenshot](Screenshot.png)
---
+++ tabler/__init__.py
@@ -0,0 +1,45 @@
+import sys
+from PySide6.QtCore import Qt
+from PySide6.QtWidgets import (
+    QApplication, QLabel, QHBoxLayout, QVBoxLayout, QWidget, QFrame, QScrollArea
+)
+
+__all__ = ['tabler']
+
+def tabler(rows, title=None):
+    assert len(rows) >= 1, 'No header row'
+    assert all(len(row) == len(rows[0]) for row in rows), "Cols don't line up"
+
+    app = QApplication.instance() or QApplication([])
+
+    main_content = QWidget()
+    main_layout = QHBoxLayout()
+    scroll_area = QScrollArea()
+    scroll_area.setWidgetResizable(True)
+    scroll_area.setWidget(main_content)
+
+    column_widgets = []
+
+    header = rows.pop(0)
+
+    for i, col in enumerate(header):
+        if i > 0:
+            main_layout.addWidget(
+                QFrame(frameShape=QFrame.VLine, frameShadow=QFrame.Sunken)
+            )
+
+        column_widget = QVBoxLayout()
+        column_widget.setAlignment(Qt.AlignTop)
+        main_layout.addLayout(column_widget)
+        column_widgets.append(column_widget)
+        column_widget.addWidget(QLabel(f'<b>{col}</b>'))
+
+    for row in rows:
+        for i, col in enumerate(row):
+            column_widget = column_widgets[i]
+            column_widget.addWidget(QLabel(col))
+
+    main_content.setLayout(main_layout)
+    scroll_area.setWindowTitle(title or "Tabler")
+    scroll_area.show()
+    app.exec_()
